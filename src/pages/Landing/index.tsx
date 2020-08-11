ca12bb7b6c98449727d07d7a2600d684a275c0f4
Marcelo Bueno <marcelo.bueno@planejamento.gov.br>
2020-08-11 14:57:04 -0300

    Finalizaçao do front-end pra versao web.

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -10,7 +10,19 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
+import api from '../../services/api';
+
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const {total} = response.data;
+
+      setTotalConnections(total);
+    })
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content">
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas.
+          Total de {totalConnections} conexões já realizadas.
           <img src={purpleHeartIcon} alt="Coração"/>
         </span>
       </div>
